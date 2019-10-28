c348c80f52f26dc1977240c910cf94408b282606
Andrew Spencer <andrew@heysparkbox.com>
2019-10-28 00:03:27 -0400

    feat: Add fonts, base typography mixins, and some font classes
    - Use the Google fonts plugin for Gatsby to get the fonts
    - Setup mixins with base font styles
    - Setup classes for reversed typography in the homepage hero
    - Remove the default site font and declear our custom font on the body tag

--- src/components/homepageHero.js
+++ src/components/homepageHero.js
@@ -4,11 +4,11 @@ const HomepageHero = () => (
   <section class="homepage-hero">
     <div class="homepage-hero__primary">
       <div class="homepage-hero__text-column">
-        <h1 class="o-heading-1">Web Designer & Developer</h1>
+        <h1 class="c-heading-1--reverse">Web Designer & Developer</h1>
 
-        <p>Andrew Spencer goes by many titles. UX/UI'er, Web Builder, Creative Developer, Frontend Designer, etc. Oh, and he enjoys branding as well.</p>
-        <p>Andrew lives in that weird space between design and code, carrying a unique balance of creativity and logic. He enjoys solving complex problems for clients and their users—with both design and code.</p>
-        <p>In his free time Andrew enjoys traveling with his wife and photographing either their adventures or their our puppy, Piper. Whether they are exploring a new city or National Park, he is always on a quest for the perfect cup of coffee and tacos—to be enjoyed separately.</p>
+        <p class="c-paragraph--reverse">Andrew Spencer goes by many titles. UX/UI'er, Web Builder, Creative Developer, Frontend Designer, etc. Oh, and he enjoys branding as well.</p>
+        <p class="c-paragraph--reverse">Andrew lives in that weird space between design and code, carrying a unique balance of creativity and logic. He enjoys solving complex problems for clients and their users—with both design and code.</p>
+        <p class="c-paragraph--reverse">In his free time Andrew enjoys traveling with his wife and photographing either their adventures or their our puppy, Piper. Whether they are exploring a new city or National Park, he is always on a quest for the perfect cup of coffee and tacos—to be enjoyed separately.</p>
       </div>
     </div>
     <div class="homepage-hero__secondary">
