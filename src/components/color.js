0ae0f91c7cdcaa5f38688ede2e5e6efea9091fdd
Andrew Spencer <andrew@heysparkbox.com>
2019-12-29 13:40:35 -0500

    refactor: Namespace color variables with --c-
    - Add the color variable code to the pattern library examples

--- src/components/color.js
+++ src/components/color.js
@@ -2,8 +2,9 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Color = ({ colorName }) => (
-  <li className="c-color-block" style={{ color: `var(--` + colorName + `)`}}>
-    <span style={{ color: `var(--stone-dark)` }}>{colorName}</span>
+  <li className="c-color-block" style={{ color: `var(--c-` + colorName + `)`}}>
+    <span className="c-color-block__name">{colorName}</span>
+    <span className="c-color-block__variable">var(--c-{colorName})</span>
   </li>
 )
 
