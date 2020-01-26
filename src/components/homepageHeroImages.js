966786108db067f1b62b19ef8a3acaf3ff1f82ae
Andrew Spencer <andrew@heysparkbox.com>
2020-01-26 18:02:08 -0500

    feat: Load animation for homepage hero images

--- src/components/homepageHeroImages.js
+++ src/components/homepageHeroImages.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import {CSSTransition} from 'react-transition-group';
 import Img from "gatsby-image"
 import Parallax from 'react-rellax'
 
@@ -33,23 +34,44 @@ const HomepageHeroImages = () => {
   return (
     <div className="c-homepage-hero__images-column">
       <div className="c-homepage-hero__images-column-inner">
-        <div className="c-homepage-hero__image-container">
-          <Parallax speed={10}>
-            <Img fluid={data.heroYoPortfolio.childImageSharp.fluid} alt="YoPortfolio website screenshot on a mobile device."/>
-          </Parallax>
-        </div>
-        <div className="c-homepage-hero__image-container c-homepage-hero__image-container--alt">
-          <Parallax speed={6}>
-            <Img fluid={data.heroTommyJohn.childImageSharp.fluid} alt="Whitworth University website screenshot on a mobile device with the navigation open."/>
-          </Parallax>
-        </div>
+        <CSSTransition
+          in={true}
+          timeout={3000}
+          classNames="c-homepage-hero__image-1"
+          appear
+        >
+          <div className="c-homepage-hero__image-container">
+            <Parallax speed={10}>
+              <Img fluid={data.heroYoPortfolio.childImageSharp.fluid} alt="YoPortfolio website screenshot on a mobile device."/>
+            </Parallax>
+          </div>
+        </CSSTransition>
+        <CSSTransition
+          in={true}
+          timeout={3000}
+          classNames="c-homepage-hero__image-2"
+          appear
+        >
+          <div className="c-homepage-hero__image-container c-homepage-hero__image-container--alt">
+            <Parallax speed={6}>
+              <Img fluid={data.heroTommyJohn.childImageSharp.fluid} alt="Whitworth University website screenshot on a mobile device with the navigation open."/>
+            </Parallax>
+          </div>
+        </CSSTransition>
       </div>
       <div className="c-homepage-hero__images-column-inner c-homepage-hero__images-column-inner--alt">
-        <div className="c-homepage-hero__image-container">
-          <Parallax speed={8}>
-            <Img fluid={data.heroNCSBN.childImageSharp.fluid} alt="NCSBN website screenshot on a mobile device."/>
-          </Parallax>
-        </div>
+        <CSSTransition
+          in={true}
+          timeout={3000}
+          classNames="c-homepage-hero__image-3"
+          appear
+        >
+          <div className="c-homepage-hero__image-container">
+            <Parallax speed={8}>
+              <Img fluid={data.heroNCSBN.childImageSharp.fluid} alt="NCSBN website screenshot on a mobile device."/>
+            </Parallax>
+          </div>
+        </CSSTransition>
       </div>
     </div>
   )
