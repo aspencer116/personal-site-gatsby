204310da1fcfee060065cda299cedd81f364b7e5
Andrew Spencer <andrew@heysparkbox.com>
2021-12-05 15:53:36 -0500

    feat: Update current work and resume

--- src/components/home/currentWork.js
+++ src/components/home/currentWork.js
@@ -4,13 +4,27 @@ const CurrentWork = () => (
   <section className="c-current-work">
     <div className="c-current-work__inner">
       <h2 className="t-meta t-meta--small c-current-work__meta">Currently</h2>
+      <span className="t-heading-4 c-current-work__title">Product Designer</span>
+      <span className="t-heading-5 c-current-work__sub-title">
+        @
+        <a className="o-delink" href="https://thoughtbot.com/" target="_blank" rel="noopener noreferrer">thoughtbot</a>
+        —a digital product consultancy
+      </span>
+      <p className="t-paragraph t-paragraph--callout">Helping organizations kickstart and scale their products by providing expertise in UX, product design, and frontend&nbsp;development.</p>
+    </div>
+
+    <div className="c-current-work__inner">
+      <h2 className="t-meta t-meta--small c-current-work__meta">Previously</h2>
       <span className="t-heading-4 c-current-work__title">Frontend Designer</span>
       <span className="t-heading-5 c-current-work__sub-title">
         @
         <a className="o-delink" href="https://sparkbox.com/" target="_blank" rel="noopener noreferrer">Sparkbox</a>
-        —a web agency
+        —a web consultancy
       </span>
-      <p className="t-paragraph t-paragraph--callout">Planning, designing, and helping to build a design system and product for a growing&nbsp;startup.</p>
+      <ul>
+        <li className="t-list t-paragraph">Worked as a designer who also writes code, helping clients ship everything from digital products and design systems to marketing and e-commerce sites while bridging design and development&nbsp;teams.</li>
+        <li className="t-list t-paragraph">Provided expertise in UI design, animation, accessibility, HTML, and CSS while balancing multiple clients and helping them solve user and business&nbsp;problems.</li>
+      </ul>
     </div>
   </section>
 )
