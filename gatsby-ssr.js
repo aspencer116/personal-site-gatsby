f92615a158c385e17368c1908798967e363bda20
Andrew Spencer <andrew@heysparkbox.com>
2023-04-29 15:26:39 -0400

    feat: Update fonts to NEXT Pan and Segment

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,26 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react';
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      key="next-pan"
+      rel="preload"
+      href="/fonts/NEXT-Pan-Book-Light.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key="next-pan-medium"
+      rel="preload"
+      href="/fonts/NEXT-Pan-Book-Medium.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+  ]);
+};
