c6b4ce0df0844ee87663596aa225890293b15fa2
Andrew Spencer <andrew@heysparkbox.com>
2023-06-17 19:35:09 -0400

    feat: New footer design

--- src/components/siteFooterLink.js
+++ src/components/siteFooterLink.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const SiteFooterLink = ({linkText, linkPath, newTab}) => (
-  <li className="c-site-footer__list-item">
+  <li className="c-site-footer__list-item t-paragraph">
     <a
       href={linkPath}
       className="c-site-footer__link"
@@ -9,6 +9,7 @@ const SiteFooterLink = ({linkText, linkPath, newTab}) => (
       rel="noopener noreferrer"
     >
       { linkText }
+      <svg className="c-external-icon" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 1.44995H8.55V9.99995M8.1 1.89995L0.45 9.54995" stroke="#0B0C0C"/></svg>
     </a>
   </li>
 )
