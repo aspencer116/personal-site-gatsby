63d76e37a4333247eeeccabc9c920ba02bc08f45
Andrew Spencer <andrew@heysparkbox.com>
2023-06-30 00:03:22 -0400

    feat: Update hero layout and styles

--- src/components/homepageHero.js
+++ src/components/homepageHero.js
@@ -5,51 +5,65 @@ import HomepageHeroImages from "./homepageHeroImages"
 const HomepageHero = () => {
   return (
     <section className="c-homepage-hero">
+      <CSSTransition
+        in={true}
+        timeout={3000}
+        classNames="transition-homepage-hero__heading"
+        appear
+      >
+        <h1 className="t-title c-homepage-hero__heading" aria-label="Product Designer">
+          <span>P</span>
+          <span>r</span>
+          <span>o</span>
+          <span>d</span>
+          <span>u</span>
+          <span>c</span>
+          <span>t</span>
+          <span className="with-space"></span>
+          <span>D</span>
+          <span>e</span>
+          <span>s</span>
+          <span>i</span>
+          <span>g</span>
+          <span>n</span>
+          <span>e</span>
+          <span>r</span>
+        </h1>
+      </CSSTransition>
       <div className="c-homepage-hero__primary">
-        <div className="c-homepage-hero__text-column">
-          <CSSTransition
-            in={true}
-            timeout={3000}
-            classNames="transition-homepage-hero__heading"
-            appear
-          >
-            <h1 className="t-title c-homepage-hero__heading">
-              <span>D</span>
-              <span>e</span>
-              <span>s</span>
-              <span>i</span>
-              <span>g</span>
-              <span className="with-space">n</span>
-              <br/>
-              <span className="with-space">&</span>
-              <span>C</span>
-              <span>o</span>
-              <span>d</span>
-              <span>e</span>
-            </h1>
-          </CSSTransition>
 
-          <CSSTransition
-            in={true}
-            timeout={5000}
-            classNames="transition-homepage-hero__text"
-            appear
-          >
-            <div className="c-homepage-hero__text-container">
-              <p className="t-paragraph t-paragraph--callout">Hello, I’m Andrew, a creative problem solver who works at the intersection of design and&nbsp;code.</p>
-              <p className="t-paragraph">Fueled by coffee and a desire to solve problems while learning something new in the process, my interests are broad and extend on both sides of the designer and developer spectrum. My true passion lies in the gray area where those two worlds collide—user-centered design, HTML, and CSS. You might call this a unicorn, frontend designer, or design engineer but I call it my happy&nbsp;place.</p>
-              <p className="t-paragraph">I’ve designed and built everything from non-profit and ecommerce websites to design systems and digital products. I strive to balance user needs, client goals, and team dynamics and work with empathy, methodical creativity, and an eye for&nbsp;design.</p>
-              <p className="t-paragraph">In my free time I enjoy traveling with my wife and photographing our adventures—or our puppy. Whether we are exploring a new city or a national park, I’m always searching for the best coffee and tacos—to be enjoyed&nbsp;separately.</p>
+        <CSSTransition
+          in={true}
+          timeout={5000}
+          classNames="transition-homepage-hero__text"
+          appear
+        >
+          <div className="c-homepage-hero__text-container">
+            <p className="c-homepage-hero__intro">Helping teams deliver successful, beautiful, and engaging digital&nbsp;products.</p>
+            <div className="o-relative c-homepage-hero__detail-container">
+              <svg className="c-homepage-hero__icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"><g clip-path="url(#a)"><path stroke="#0B0C0C" stroke-width="1.5" d="M12.344 2.738h6.906v9.5H6.39v-9.5h5.954Z"/><path stroke="#0B0C0C" stroke-width="1.5" d="m19.648 12.271-4.74 4.741H2.066l4.846-4.893"/><path fill="#0B0C0C" d="M13.014 5.013h4v4h-4z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h20v20H0z"/></clipPath></defs></svg>
+              <span className="t-meta t-meta--small c-homepage-hero__detail">Senior Product Designer @<a href="https://thoughtbot.com">thoughtbot</a></span>
             </div>
-          </CSSTransition>
-        </div>
-        <HomepageHeroImages />
+            <div className="o-relative c-homepage-hero__detail-container">
+              <svg className="c-homepage-hero__icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"><path stroke="#0B0C0C" stroke-width="1.5" d="M10 3.75h9.25v13.5H.75V12"/><path fill="#0B0C0C" d="m1.582 9.997 4-7.966 4 7.966h-8ZM4.582 9.997h2v2h-2z"/></svg>
+              <span className="t-meta t-meta--small c-homepage-hero__detail">New Englander</span>
+            </div>
+            <div className="o-relative c-homepage-hero__detail-container">
+              <svg className="c-homepage-hero__icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"><path stroke="#0B0C0C" stroke-width="1.5" d="M1.276 10.02h17.448M10.059 0v20"/><path stroke="#0B0C0C" stroke-width="1.6" d="m3.504 3.458 13.077 13.077M3.381 16.469 16.458 3.392"/><rect width="5.898" height="5.898" x="7.051" y="7.051" fill="#0B0C0C" rx="2.949"/></svg>
+              <span className="t-meta t-meta--small c-homepage-hero__detail">Design generalist, product specialist</span>
+              <ul className="o-delistelement c-homepage-hero__list">
+                <li className="o-delist t-meta t-meta--small">UI/UX Design</li>
+                <li className="o-delist t-meta t-meta--small">Design Ops</li>
+                <li className="o-delist t-meta t-meta--small">Design Systems</li>
+                <li className="o-delist t-meta t-meta--small">Web Design</li>
+                <li className="o-delist t-meta t-meta--small">Frontend Dev</li>
+                <li className="o-delist t-meta t-meta--small">Photography</li>
+              </ul>
+            </div>
+          </div>
+        </CSSTransition>
       </div>
-      <div className="c-homepage-hero__secondary">
-        <p className="t-meta c-homepage-hero__meta-text">Boston&nbsp;MA</p>
-      </div>
-      <div className="c-homepage-hero__tertiary">
-      </div>
+      <HomepageHeroImages />
     </section>
   )
 }
