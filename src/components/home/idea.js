7009aa21805fd18e0c5fc88f792659eadb92c462
Andrew Spencer <andrew@heysparkbox.com>
2023-07-04 15:07:20 -0400

    Fix console errors

--- src/components/home/idea.js
+++ src/components/home/idea.js
@@ -1,9 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 
 const Idea = ({callout, type, title, forName, actionLink, actionText}) => (
   <li className="o-delist">
-    <Link target="_blank" to={actionLink} className={`o-delink c-idea ${callout ? 'c-idea--callout' : ''}`}>
+    <a target="_blank" href={actionLink} className={`o-delink c-idea ${callout ? 'c-idea--callout' : ''}`}>
       <div className="c-idea__top">
         <span className="t-meta t-meta--small t-subtle c-idea__type">{ type }</span>
         <h3 className="t-heading-4 c-idea__title">{ title }</h3>
@@ -19,7 +18,7 @@ const Idea = ({callout, type, title, forName, actionLink, actionText}) => (
           <svg className="c-idea__icon" width="10" height="10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 .5h9.5V10M9 1l-9 9" stroke="inherit"/></svg>
         </div>
       </div>
-    </Link>
+    </a>
   </li>
 )
 
