88894c9eb56a59106d0e531555df07de5b3496af
Andrew Spencer <aspencer.hello@gmail.com>
2023-07-16 17:00:14 -0400

    Add Work section to homepage
    - Adds a new section for work to the homepage
    - Adjusted typography styles
    - Adjusted Ideas section styles

--- src/components/home/idea.js
+++ src/components/home/idea.js
@@ -5,16 +5,16 @@ const Idea = ({callout, type, title, forName, actionLink, actionText}) => (
     <a target="_blank" rel="noreferrer" href={actionLink} className={`o-delink c-idea ${callout ? 'c-idea--callout' : ''}`}>
       <div className="c-idea__top">
         <span className="t-meta t-meta--small t-subtle c-idea__type">{ type }</span>
-        <h3 className="t-heading-4 c-idea__title">{ title }</h3>
+        <h3 className="t-heading-3 c-idea__title">{ title }</h3>
       </div>
       <div className="c-idea__bottom">
         {forName &&
           <div>
-            <span className="t-paragraph--small t-tight t-subtle">for { forName }</span>
+            <span className="t-paragraph--small t-paragraph--tighter t-subtle">for { forName }</span>
           </div>
         }
         <div className="c-idea__action">
-          <span className="t-paragraph--small t-tight">{ actionText }</span>
+          <span className="t-paragraph--small t-paragraph--tighter">{ actionText }</span>
           <svg className="c-idea__icon" width="10" height="10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 .5h9.5V10M9 1l-9 9" stroke="inherit"/></svg>
         </div>
       </div>
---
+++ src/components/home/work.js
@@ -0,0 +1,90 @@
+import React from "react"
+import WorkListing from "./workListing"
+
+const Work = () => (
+  <section className="c-work">
+      <h2 className="t-title c-work__title">Work</h2>
+      <div className="c-work__text">
+        <p className="t-paragraph">Having spent most of my career working at digital agencies, I’ve developed a broad skillset while working alongside some incredibly talented people on a diverse array of&nbsp;projects.</p>
+        <p className="t-paragraph t-subtle">Case studies coming <s>soon</s> someday.</p>
+      </div>
+      <ul className="u-delist c-work__listings">
+        <WorkListing
+          title="Harvard Business Review"
+          description="A complete product redesign to help leaders discover and engage with&nbsp;HBR."
+          date="2023"
+          tags={["Product Design Lead", "User Research"]}
+        />
+        <WorkListing
+          title="Groups Recover Together"
+          description="A pivot to a digital platform for an organization providing hope to those recovering from&nbsp;addiction."
+          date="2022"
+          tags={["Product Design"]}
+        />
+        <WorkListing
+          title="Fanatics"
+          description="A new consumer app for the leader in licensed sports&nbsp;merchandise."
+          date="2022"
+          tags={["Product Design"]}
+        />
+        <WorkListing
+          title="National Kidney Foundation"
+          description="A website redesign to help patients and professionals find the information they&nbsp;need."
+          date="2019"
+          tags={["Design", "Frontend Dev"]}
+          liveSite={"https://www.kidney.org"}
+        />
+        <WorkListing
+          title="Seegrid"
+          description="An ecosystem of digital tools to help humans manage warehouse automation&nbsp;robots."
+          date="2021"
+          tags={["Product Design", "Design System"]}
+        />
+        <WorkListing
+          title="Dealer Tire"
+          description="A first of its kind product to help car dealerships inspect, track, and sell&nbsp;tires."
+          date="2019"
+          tags={["Design", "Frontend Dev"]}
+        />
+        <WorkListing
+          title="Frontrow Health"
+          description="A zero to MVP launch for a health tech startup delivering a health products marketplace for consumers powered by reviews from&nbsp;providers."
+          date="2022"
+          tags={["Product Design"]}
+        />
+        <WorkListing
+          title="Prolegis"
+          description="A variety of features for a startup delivering digital policy tools for Capitol&nbsp;Hill."
+          date="2023"
+          tags={["Product Design"]}
+        />
+        <WorkListing
+          title="Design System Survey"
+          description="A visual guide to the latest on design&nbsp;systems."
+          date="2021"
+          tags={["Web Design", "Branding"]}
+          liveSite="https://designsystemsurvey.sparkbox.com/2021/"
+        />
+        {/* <WorkListing
+          title="Shoes for Crews"
+          description="A complete redesign of a multi-brand e-commerce&nbsp;website."
+          date="2019"
+          tags={["Web Design", "Frontend Dev"]}
+        /> */}
+        {/* <WorkListing
+          title="YoPortfolio"
+          description="A playful brand and MVP product for a startup stock market&nbsp;tool."
+          date="2017"
+          tags={["Branding", "Product Design"]}
+        />
+        <WorkListing
+          title="Tommy John"
+          description="A checkout flow optimized for an emerging online clothing&nbsp;brand."
+          date="2019"
+          tags={["Web Design", "Frontend Dev"]}
+        /> */}
+      </ul>
+  </section>
+)
+
+export default Work
