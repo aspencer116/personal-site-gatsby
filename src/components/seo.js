ed368600d7b21d78ac23f5b02eaa65783ef6900a
Andrew Spencer <aspencer.hello@gmail.com>
2023-08-10 21:56:51 -0400

    Update Title and OG for SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -53,7 +53,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:image`,
-          content: site.siteMetadata.image,
+          content: '/images/opengraph.jpg',
         },
         {
           property: `og:type`,
