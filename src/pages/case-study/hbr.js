13de091c16e7b200af8ab9ff6199648aa3f14247
Andrew Spencer <aspencer.hello@gmail.com>
2024-03-09 15:31:03 -0500

    Initial page content and modify case study header

--- src/pages/case-study/hbr.js
+++ src/pages/case-study/hbr.js
@@ -16,114 +16,6 @@ import Img from "gatsby-image"
 const HBR = () => {
   const data = useStaticQuery(graphql`
     query {
-      yoPortfolioSketch1: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_sketch_1.jpg" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioSketch2: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_sketch_2.jpg" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioBrand1: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_brand_1.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 2000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioBrand2: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_brand_2.jpg" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 2000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioBrand3: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_brand_3.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 1400) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioBrand4: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_brand_4.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 1400) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioUI1: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_UI_1.jpg" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 1400) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioUI2: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_UI_2.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 1400) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioUI5: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_UI_5.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 2000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioUI66: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_UI_66.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 2000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioUIgif: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_UI_gif.gif" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 2000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      yoPortfolioUI7: file(
-        relativePath: { eq: "yo-portfolio/yo-portfolio_UI_7.png" }
-      ) {
-        childImageSharp {
-          fluid(maxWidth: 2000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
       HBRcomponents: file(relativePath: { eq: "hbr/HBR_components.png" }) {
         childImageSharp {
           fluid(maxWidth: 2400) {
@@ -132,6 +24,13 @@ const HBR = () => {
         }
       }
       HBRheader: file(relativePath: { eq: "hbr/HBR_mockup-1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 2400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+      HBRgrid: file(relativePath: { eq: "hbr/HBR_grid-1.png" }) {
         childImageSharp {
           fluid(maxWidth: 2400) {
             ...GatsbyImageSharpFluid
@@ -153,6 +52,7 @@ const HBR = () => {
           title="Harvard Business Review"
           subtitle="Reinventing the HBR mobile app for the next generation of business&nbsp;leaders"
           year="2023"
+          agency="thoughtbot"
           role="Product Design Lead"
           skills={["Design Sprint", "User Research", "React Native"]}
           imageAltText="Person holding a phone in their lap looking at the Harvard Business Review mobile app."
@@ -177,6 +77,96 @@ const HBR = () => {
       </section>
 
       <div className="c-case-study">
+        <div className="o-text-width-limiter c-case-study__section">
+          <p className="t-paragraph t-paragraph--callout">
+            The Harvard Business Review knew the time had come to overhaul their mobile app. The goal was a full re-envisioning of the experience to engage a younger audience and align the look and feel of the app with the HBR brand.
+          </p>
+        </div>
+
+        <div className="c-case-study__section c-case-study__full-width-image">
+          <Img
+            fluid={data.HBRgrid.childImageSharp.fluid}
+            alt="Screenshots of the new HBR mobile app arranged in a grid"
+            class="c-case-study__image"
+          />
+        </div>
+        
+        <div className="o-text-width-limiter c-case-study__section">
+          <h2 className="t-heading-1">How it started</h2>
+          <p className="t-paragraph">
+            The Harvard Business Review called on <a href="https://thoughtbot.com/">thoughtbot</a>, the agency where I work, to help them build a new mobile app with a clear objective—design a new app that was worthy of the HBR brand. An MVP that didn’t look or feel like an MVP.
+          </p>
+          <p className="t-paragraph">
+          We knew we had to nail the fundamentals of a media app with features like browsing and reading articles, but we wanted to also make sure we could engage with the audience in a way that was personal and habit-building.
+          </p>
+        </div>
+
+        <CaseStudyVideo
+          videoURL={videoLightDark}
+          ariaLabel="Screenshots of the HBR mobile app that switch between light and dark mode"
+        />
+        
+        <div className="o-text-width-limiter c-case-study__section">
+          <h2 className="t-heading-1">My role</h2>
+          <p className="t-paragraph">
+            My role as the lead product designer was broad. I facilitated a design sprint, ran user interviews, orchestrated the UX of the app, collaborated with the HBR design team on a UI direction, and even jumped into code to help make it all a reality. This was a team effort every step of the way and I worked closely with another thoughtbot designer, two HBR designers, and a talented team of developers and product managers.
+          </p>
+        </div>
+
+        <div className="c-case-study__section c-case-study__section--2-column">
+          <div className="c-case-study__image-container">
+            <Img
+              fluid={data.HBRcomponents.childImageSharp.fluid}
+              alt="Sketchbook of YoPortfolio brand ideas opened and held in a hand."
+              class="c-case-study__image"
+            />
+          </div>
+          <div className="c-case-study__caption reverse">
+            <div className="c-case-study__sticky-text">
+              <p className="t-paragraph t-paragraph--small">
+                To kick off the work on a new app, thoughtbot ran a design sprint with the HBR team to gain a better understanding of the work that had been done up to this point and what different internal stakeholders envisioned for the new app.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        [ image of user testing flows ]
+
+        <div className="o-text-width-limiter c-case-study__section">
+          <p className="t-paragraph">
+            The design team facilitated user interviews with people in our target audience to better understand the features that were important to our users and how we should structure the application.
+          </p>
+        </div>
+
+        [ iterations of the homescreen ]
+
+        <div className="o-text-width-limiter c-case-study__section">
+          <p className="t-paragraph">
+            User testing helped us validate, or invalidate, our assumptions about how the navigation should be structured, what content should be on the home screen, and what features would help users build a habit of interacting with the app.
+          </p>
+        </div>
+
+        [ brand applied to UX ]
+
+        <div className="o-text-width-limiter c-case-study__section">
+          <p className="t-paragraph">
+            As Lead Product Designer, I proposed a team structure and process that allowed the team to work effectively and cater to everyone’s strengths. The HBR design team owned the UI since they knew the brand in and out, and thoughtbot designers owned the UX since we had more experience shipping products. We all collaborated often to share ideas and eventually integrated our work into a single product vision.
+          </p>
+          <p className="t-paragraph">
+            The thoughtbot designers also jumped into the React Native code to help developers refine UI details and close the long feedback loops that often exist between design and engineering teams. This allowed us to move faster while still upholding a high visual quality.
+          </p>
+        </div>
+
+        <div className="o-text-width-limiter c-case-study__section">
+          <h2 className="t-heading-1">Results</h2>
+          <p className="t-paragraph">
+            At the end of the project, the HBR team said this was the best experience they had ever had working with outside consultants. We also were able to deliver and launch an MVP that doesn’t look like an MVP—something all of the designers appreciated.
+          </p>
+          [ quote from HBR ]
+        </div>
+
+
+
         <div className="o-text-width-limiter c-case-study__section">
           <p className="t-paragraph t-paragraph--callout">
             The Harvard Business Review knew the time had come to overhaul their
@@ -205,12 +195,12 @@ const HBR = () => {
         <div className="c-case-study__section c-case-study__section--2-column">
           <div className="c-case-study__image-container">
             <Img
-              fluid={data.yoPortfolioSketch1.childImageSharp.fluid}
+              fluid={data.HBRcomponents.childImageSharp.fluid}
               alt="Sketchbook of YoPortfolio brand ideas opened and held in a hand."
               class="c-case-study__image"
             />
             <Img
-              fluid={data.yoPortfolioSketch2.childImageSharp.fluid}
+              fluid={data.HBRcomponents.childImageSharp.fluid}
               alt="YoPortfolio brand sketches."
               class="c-case-study__image"
             />
@@ -259,7 +249,7 @@ const HBR = () => {
           </div>
           <div className="c-case-study__full-width-image">
             <Img
-              fluid={data.yoPortfolioBrand1.childImageSharp.fluid}
+              fluid={data.HBRcomponents.childImageSharp.fluid}
               alt="Various YoPortfolio brand explorations with different design styles."
               class="c-case-study__image"
             />
@@ -285,22 +275,17 @@ const HBR = () => {
           </div>
           <div className="c-case-study__image-container">
             <Img
-              fluid={data.yoPortfolioUI1.childImageSharp.fluid}
+              fluid={data.HBRcomponents.childImageSharp.fluid}
               alt="Various design elements of YoPortfolio shown on a monitor screen."
               class="c-case-study__image"
             />
           </div>
         </div>
 
-        <CaseStudyVideo
-          videoURL={videoLightDark}
-          ariaLabel="Screenshots of the HBR mobile app that switch between light and dark mode"
-        />
-
         <div className="c-case-study__section c-case-study__section--2-column">
           <div className="c-case-study__image-container">
             <Img
-              fluid={data.yoPortfolioUI1.childImageSharp.fluid}
+              fluid={data.HBRcomponents.childImageSharp.fluid}
               alt="Various design elements of YoPortfolio shown on a monitor screen."
               class="c-case-study__image"
             />
