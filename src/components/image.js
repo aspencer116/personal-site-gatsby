8048c3de7c4389f886991b5d070a4c8bce7d9f1f
Andrew Spencer <aspencer.hello@gmail.com>
2025-02-20 21:33:27 -0500

    Remove unused case studies

--- src/components/image.js
+++ src/components/image.js
@@ -1,13 +1,13 @@
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 /*
- * This component is built using `gatsby-image` to automatically serve optimized
+ * This component is built using `gatsby-plugin-image` to automatically serve optimized
  * images with lazy loading and reduced file sizes. The image is loaded using a
  * `useStaticQuery`, which allows us to load the image from directly within this
  * component, rather than having to pass the image data down from pages.
  *
  * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `gatsby-plugin-image`: https://gatsby.dev/gatsby-plugin-image
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
